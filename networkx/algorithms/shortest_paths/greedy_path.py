5e71636868e79735ce217ff84981cb0f8f291896
andrea_97 <aracri.andrea@gmail.com>
2021-03-31 20:45:54 +0200

    implemented greedy search

---
+++ networkx/algorithms/shortest_paths/greedy_path.py
@@ -0,0 +1,64 @@
+import networkx as nx
+from itertools import count
+from heapq import heappop, heappush
+from networkx.algorithms.shortest_paths.weighted import _weight_function
+
+__all__ = ['greedy_path']
+
+def greedy_path(G, source, target, heuristic=None, weight="weight"):
+    if (source not in G or target not in G):
+        msg = f"Either source {source} or target {target} is not in G"
+        raise nx.NodeNotFound(msg)
+
+    push = heappush
+    pop = heappop
+
+    c = count()
+    queue = [(0, next(c), source, 0, None)]
+    
+    enqueued = {}
+    explored = {}
+
+    # Using weight if heuristic is None
+    weight = _weight_function(G, weight)
+    if heuristic is None:
+        def heuristic(u,v):
+            cost, _ = enqueued[u]
+            return cost
+
+    while queue:
+        _, __, curnode, dist, parent = pop(queue)
+
+        if curnode == target:
+            path = [curnode]
+            node = parent
+            while node is not None:
+                path.append(node)
+                node = explored[node]
+            path.reverse()
+            return path
+        
+        if curnode in explored:
+            if explored[curnode] is None:
+                continue
+
+            qcost, h = enqueued[curnode]
+            if qcost < dist:
+                continue
+        
+        explored[curnode] = parent
+
+        for neighbor, w in G[curnode].items():
+            ncost = dist + weight(curnode, neighbor, w)
+            if neighbor in enqueued:
+                qcost, h = enqueued[neighbor]
+                
+                if qcost <= ncost:
+                    continue
+            else:
+                # setting costs in case heuristic is None
+                enqueued[neighbor] = ncost, ncost
+                h = heuristic(neighbor, target)
+            enqueued[neighbor] = ncost, h
+            push(queue, (h, next(c), neighbor, ncost, curnode))
+    raise nx.NetworkXNoPath(f"Node {target} not reachable from {source}")
